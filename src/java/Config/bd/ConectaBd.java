11caa354a56b0ab82c1c05185c0b1affd84806fa
Alex palacios <davixdmarzo97@gmail.com>
2020-08-03 12:12:52 -0500

    Creacion clase usuario

--- src/java/Config/bd/ConectaBd.java
+++ src/java/Config/bd/ConectaBd.java
@@ -18,7 +18,7 @@ public class ConectaBd {
         }
         return cn;
     }
-    private final String url = "jdbc:mysql://localhost:3306/sugerencias";
+    private final String url = "jdbc:mysql://localhost:3306/sugerencia";
     private final String driver = "com.mysql.jdbc.Driver";
     private final String user = "root";
     private final String password = "123";
--- src/java/Modelo/Usuario.java
+++ src/java/Modelo/Usuario.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
 /**
@@ -10,5 +6,62 @@ package Modelo;
  * @author Alex
  */
 public class Usuario {
+    private int idusuario;
+    private String nombre;
+    private String usuario;
+    private String contrasenia;
+    private String estado;
+
+    public Usuario() {
+    }
+
+    public Usuario(int idusuario, String nombre, String usuario, String contrasenia, String estado) {
+        this.idusuario = idusuario;
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.estado = estado;
+    }
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
     
 }
